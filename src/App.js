315d62f83a323ecf00cf32e4fbe477341f2ac9bd
Srinivash Dev <srinnivassh@gmail.com>
2023-11-19 19:39:30 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import Bar from "./components/Bar";
+
+import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.cjs.jsx';
+
+
+
 
 
 
 function App() {
   return (
     <div className=" text-red-600">
-     <h1 className="text-blue-600">Srinivash</h1>
+     <Bar/>
+     
+     <ScrollyVideo src="1.mp4"/>
+     
     </div>
   );
 }
---
+++ src/components/Bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bar = () => {
+  return (
+    <div className='text-pink-700'>
+      <h1>Srinivash</h1>
+    </div>
+  )
+}
+
+export default Bar
